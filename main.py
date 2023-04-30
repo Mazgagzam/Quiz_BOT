fad4d0434592737508a392b0b4d3dbc7018d4401
Mazgagzam <94379179+Mazgagzam@users.noreply.github.com>
2023-05-01 01:27:36 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,43 @@
+import asyncio
+
+import logging
+
+from aiogram import Bot, Dispatcher
+
+from handlers import register_func
+
+from database.database_class import DataBase
+
+from database.db_arg import db
+
+from learn_maks import *
+
+logging.basicConfig(level=logging.INFO)
+
+async def main():
+
+    bot = Bot(token="5340042795:AAE6p8mpYWHwIIvA_0qW2195oSNxwkVgAuE", parse_mode = 'html')
+
+    dp = Dispatcher(bot)
+
+    register_func(dp)
+
+  
+
+    await dp.start_polling()
+
+   
+
+def cli():
+
+    try:
+
+        asyncio.run(main())
+
+    except (KeyboardInterrupt, SystemExit):
+
+        print('BOT STOPPED')
+
+if __name__ == '__main__':
+
+    cli()
